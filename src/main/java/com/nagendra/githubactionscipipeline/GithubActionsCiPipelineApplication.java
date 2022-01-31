8cc41067460bbccf4d72eabbc2922dfe700d66a6
nagendrakumar <inbox.nagendra@gmail.com>
2022-01-31 13:02:20 +0530

    Feat: Initial setup

---
+++ src/main/java/com/nagendra/githubactionscipipeline/GithubActionsCiPipelineApplication.java
@@ -0,0 +1,13 @@
+package com.nagendra.githubactionscipipeline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GithubActionsCiPipelineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GithubActionsCiPipelineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/nagendra/githubactionscipipeline/GithubActionsCiPipelineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nagendra.githubactionscipipeline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GithubActionsCiPipelineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
